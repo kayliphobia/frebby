364683015de36f635123eac823703e02dd2f8a69
el <52547253+crELu@users.noreply.github.com>
2025-11-25 00:00:12 -0800

    fixed day timer back to normal, made the female only move at day 2

--- Assets/scripts/FemaleAI.cs
+++ Assets/scripts/FemaleAI.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class FemaleAI : AI
-{    
+{
+    protected override void Update()
+    {
+        if (gameManager.getCurrentDay() < 2)
+        {
+            AILevel = 0;
+        }
+        base.Update();
+    }
 }
